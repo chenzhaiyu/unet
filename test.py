3c3f3293c7b0e937090e85fb33c5d74c92a5d1da
Zhaiyu <contact@chenzhaiyu.com>
2019-09-30 16:22:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ model = unet()
 model.load_weights("models/unet_buildings_weights.14-0.04.hdf5", by_name=True)
 
 testGene = testGenerator("data/whu/test", as_gray=False, flag_multi_class=True)
-results = model.predict_generator(testGene,100,verbose=1)
-saveResult("data/whu/result",results)
+results = model.predict_generator(testGene, 100, verbose=1)
+saveResult("data/whu/result", results)
